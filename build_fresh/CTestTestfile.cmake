431ae88f11ec9e8dbf17b852e07ec7458d4ae247
TiagoBe0 <santiagobergamin@gmail.com>
2026-04-15 17:59:43 -0300

    pus

---
+++ build_fresh/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/santi-simaf/Documentos/ABRIL-2026/DistributionTool
+# Build directory: /home/santi-simaf/Documentos/ABRIL-2026/DistributionTool/build_fresh
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
---
+++ build_fresh/tests/CMakeFiles/distool_tests.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/santi-simaf/Documentos/ABRIL-2026/DistributionTool/tests/main.cpp" "tests/CMakeFiles/distool_tests.dir/main.cpp.o" "gcc" "tests/CMakeFiles/distool_tests.dir/main.cpp.o.d"
+  "/home/santi-simaf/Documentos/ABRIL-2026/DistributionTool/tests/test_classifier.cpp" "tests/CMakeFiles/distool_tests.dir/test_classifier.cpp.o" "gcc" "tests/CMakeFiles/distool_tests.dir/test_classifier.cpp.o.d"
+  "/home/santi-simaf/Documentos/ABRIL-2026/DistributionTool/tests/test_descriptors.cpp" "tests/CMakeFiles/distool_tests.dir/test_descriptors.cpp.o" "gcc" "tests/CMakeFiles/distool_tests.dir/test_descriptors.cpp.o.d"
+  "/home/santi-simaf/Documentos/ABRIL-2026/DistributionTool/tests/test_pca.cpp" "tests/CMakeFiles/distool_tests.dir/test_pca.cpp.o" "gcc" "tests/CMakeFiles/distool_tests.dir/test_pca.cpp.o.d"
+  "/home/santi-simaf/Documentos/ABRIL-2026/DistributionTool/tests/test_reader.cpp" "tests/CMakeFiles/distool_tests.dir/test_reader.cpp.o" "gcc" "tests/CMakeFiles/distool_tests.dir/test_reader.cpp.o.d"
+  "/home/santi-simaf/Documentos/ABRIL-2026/DistributionTool/tests/test_statistics.cpp" "tests/CMakeFiles/distool_tests.dir/test_statistics.cpp.o" "gcc" "tests/CMakeFiles/distool_tests.dir/test_statistics.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
